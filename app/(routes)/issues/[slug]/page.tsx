1e368511e15261bb3604b42a29703d424c96d318
bvcr93 <darelbavcar1@gmail.com>
2023-12-20 17:16:41 +0100

    package.json updated

--- app/(routes)/issues/[slug]/page.tsx
+++ app/(routes)/issues/[slug]/page.tsx
@@ -1,8 +1,5 @@
 import { IssueItem } from "@/components/ui/issue-item";
 import { db } from "@/lib/db";
-import { PrismaClient } from "@prisma/client";
-
-const prisma = new PrismaClient();
 
 interface IssueDetailsProps {
   params: {
@@ -13,30 +10,19 @@ interface IssueDetailsProps {
 export default async function IssueDetailsPage({
   params: { slug },
 }: IssueDetailsProps) {
-  try {
-    // Ensure Prisma Client is generated during the build process
-    await prisma.$connect();
+  const issue = await db.issue.findUnique({
+    where: {
+      id: slug,
+    },
+  });
 
-    const issue = await prisma.issue.findUnique({
-      where: {
-        id: slug,
-      },
-    });
-
-    if (!issue) {
-      return <div>Issue not found</div>;
-    }
-
-    return (
-      <div className="maincol mt-10">
-        <IssueItem {...issue} />
-      </div>
-    );
-  } catch (error) {
-    console.error("Error fetching data:", error);
-    return <div>Error fetching data</div>;
-  } finally {
-    // Disconnect Prisma Client after fetching data
-    await prisma.$disconnect();
+  if (!issue) {
+    return <div>Issue not found</div>;
   }
+
+  return (
+    <div className="maincol mt-10">
+      <IssueItem {...issue} />
+    </div>
+  );
 }
